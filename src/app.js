8bf77ae3757000b271913bd893d107733af91e6b
Cristian-svg598 <criguies@gmail.com>
2024-10-04 10:31:22 +0000

    Generador de excusas acabado

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+/* Variables */
+let who = ["The dog", "My Grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = ["before the class", "when i was sleeping"];
+
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
@@ -7,5 +13,12 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+
+  document.getElementById("excuse").innerText = excuse;
 };
